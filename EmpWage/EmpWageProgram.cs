c486eadf3e785c0f63843e81c9f72a933e2b88a2
Amitmagadum1545 <amitmagadum1008@gmail.com>
2022-02-14 17:27:20 +0530

    [Amit] Added Switch Case

--- EmpWage/EmpWageProgram.cs
+++ EmpWage/EmpWageProgram.cs
@@ -11,25 +11,24 @@ namespace EmpWage
         public static void EmployeeDailyWage()
         {
             const int WAGEPERHOUR = 20;
-            int empWorkHr;
+            int empWorkHr=0;
             int DailyEmployeeWage = 0;
             Random random = new Random();
             int empCheck = random.Next(3);
-            if(empCheck == 0)
-            {
-                Console.WriteLine("Employee is Absent");
-                empWorkHr = 0;
-            }
-            else if (empCheck == 1)
-            {
-                Console.WriteLine("Employee is Present");
-                empWorkHr = 8;
-            }
-            else
-            {
-                Console.WriteLine("Employee is Part Time Present");
-                empWorkHr = 4;
-                
+            switch(empCheck)
+            { 
+                case 0:
+                    Console.WriteLine("Employee is Absent");
+                    empWorkHr = 0;
+                    break;
+                case 1:           
+                    Console.WriteLine("Employee is Present");
+                    empWorkHr = 8;
+                    break;
+                case 2:
+                    Console.WriteLine("Employee is Part Time Present");
+                    empWorkHr = 4;
+                    break;
             }
             DailyEmployeeWage = empWorkHr * WAGEPERHOUR;
             Console.WriteLine($"Daily Employee Wage is" + DailyEmployeeWage);
